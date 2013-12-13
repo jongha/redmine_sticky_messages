64fdb72f313cd4644c2a393bc742a8e155cef159
Jong-Ha Ahn <jongha.ahn@mrlatte.net>
2013-12-13 18:12:29 +0900

    create project

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_sticky_messages do
+  name 'Redmine Sticky Messages plugin'
+  author 'Jong-Ha Ahn'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://github.com/jongha/redmine_sticky_messages'
+  author_url 'http://www.mrlatte.net'
+end
