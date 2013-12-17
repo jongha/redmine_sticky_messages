67396fbeee1939825b505ae0476c14bf164bbeda
Jong-Ha Ahn <jongha.ahn@mrlatte.net>
2013-12-17 14:53:27 +0900

    Add Noty Style

--- assets/javascripts/sticky_messages.js
+++ assets/javascripts/sticky_messages.js
@@ -1,7 +1,7 @@
 var StickyMessagesCookie = {
     __set: function(key, values) {
         "use strict";
-        
+
         var val = "", i;
         if(values instanceof Array) {
             for(i in values) {
@@ -13,7 +13,7 @@ var StickyMessagesCookie = {
         }else {
             val = values;
         }
-        
+
         var expire = new Date();
         expire.setDate(expire.getDate() + 1); // for 1 day.
         document.cookie = (key + '=' + val + "; path=/");
@@ -26,35 +26,77 @@ var StickyMessagesCookie = {
             var c = cookies[i];
             var pos = c.indexOf("=");
             var name = pos > -1 ? c.substr(0, pos) : c;
-            
+
             document.cookie = name + "=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT";
-        }        
+        }
     },
     _set: function(key, value) {
         "use strict";
-        
+
         this.__set(key, value);
-    },    
+    },
     _get: function(key) {
         "use strict";
-        
+
         key = key + "=";
         var c = document.cookie;
         var start = c.indexOf(key);
         var val = "";
-        
+
         if(start !== -1) {
             start += key.length;
             var end = c.indexOf(";", start);
             if(end === -1) { end = c.length; }
-            
+
             val = c.substring(start, end);
         }
-        return decodeURIComponent(val);    
+        return decodeURIComponent(val);
     },
     _dispatcher: function(options) {
         "use strict";
-        
+
         return this["_" + options.type](options.key, options.data);
     }
 };
+
+
+$(document).ready(function() {
+    $.noty.defaults = {
+        layout: 'bottomRight',
+        theme: 'defaultTheme',
+        type: 'warning',
+        text: '',
+        dismissQueue: false, // If you want to use queue feature set this true
+        template: '<div class="noty_message"><span class="noty_text"></span><div class="noty_close"></div></div>',
+        animation: {
+            open: {height: 'toggle'},
+            close: {height: 'toggle'},
+            easing: 'swing',
+            speed: 200 // opening & closing animation speed
+        },
+        timeout: true, // delay for closing event. Set false for sticky notifications
+        force: false, // adds notification to the beginning of queue when set to true
+        modal: false,
+        maxVisible: 5, // you can set max visible notification for dismissQueue true option,
+        killer: false, // for close all notifications before show
+        closeWith: ['click'], // ['click', 'button', 'hover']
+        callback: {
+            onShow: function() {},
+            afterShow: function() {},
+            onClose: function() {},
+            afterClose: function() {}
+        },
+        buttons: [
+            {
+                addClass: 'btn btn-danger', text: $("#_sticky_messages-ok").text(), onClick: function($noty) {
+                    StickyMessagesCookie._dispatcher({ type: "set", key: "_sticky_messages-off", data: "1" });
+                    $noty.close();
+                }
+            }
+        ]
+    };
+    
+    if($('._sticky_messages').length > 0 && StickyMessagesCookie._dispatcher({ type: "get", key: "_sticky_messages-off" }) !== "1") {
+        noty({ text: $($('._sticky_messages')[0]).clone().show()[0].outerHTML });
+    };
+});
--- lib/sticky_messages_application_hooks.rb
+++ lib/sticky_messages_application_hooks.rb
@@ -4,7 +4,9 @@ class StickyMessagesHeaderHooks < Redmine::Hook::ViewListener
 
   def view_layouts_base_html_head(context = {})
     o = stylesheet_link_tag('sticky_messages', :plugin => 'redmine_sticky_messages')
+    o << stylesheet_link_tag('buttons', :plugin => 'redmine_sticky_messages')
     o << javascript_include_tag('sticky_messages', :plugin => 'redmine_sticky_messages')
+    o << javascript_include_tag('jquery.noty.packaged.min.js', :plugin => 'redmine_sticky_messages')
     return o
   end
 end
